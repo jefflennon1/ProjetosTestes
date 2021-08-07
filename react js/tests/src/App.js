df9b605a691513c1728f3ab15e67e3847c0ee558
jefflennon1 <jeffersonlennon1@outlook.com>
2021-08-07 18:50:20 -0300

    mais dos hooks

--- react js/tests/src/App.js
+++ react js/tests/src/App.js
@@ -2,27 +2,42 @@ import React, { useEffect, useState } from 'react';
 
 export default function App(){
   const [repos, setRepos] = useState([]);
+  const [ favorite, setFavorites] = useState([]);
 
 useEffect(()=>{
   searchrepos();
-}, [])
+}, []);
+
+useEffect(()=>{
+  handleFavoritiesTitle();
+}, [repos]);
 
 async function searchrepos(){
   const response = await fetch('https://api.github.com/users/jefflennon1/repos');
   const data = await response.json();
    setRepos(data);
 }
+function handleFavoritiesTitle(){
+    const filtered = repos.filter(repo => repo.favorite);
+    document.title = 'Você tem '+filtered.length+' favoritos';
+}
+
+function handleAddToFavorites(id){
+    const newRepo = repos.map(repo =>{
+        return repo.id === id ? {...repo, favorite : !repo.favorite} : repo;
+    });
+    setRepos(newRepo);
+}
 
   return (
-     
       <ul>
           {repos.map( repo => (
             <>
-              <li key={repo.id}>{repo.name}</li>
-                <button>Favoritar</button>
+              <li key={repo.id}>{repo.name} {repo.favorite && <span>(FAVORITE)</span>}
+                 <button  onClick={()=>handleAddToFavorites(repo.id)}>Favoritar</button>
+              </li>
             </>
           ))}
       </ul>
-     
   )
 }
