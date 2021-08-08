b9c15f57c95d12e5d1104eba2d60f8d3f4614eff
jefflennon1 <jeffersonlennon1@outlook.com>
2021-08-08 14:57:30 -0300

    atualizando a forma d epegar o hash md5

--- react js/tests/src/App.js
+++ react js/tests/src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import md5 from 'md5';
 
 export default function App(){
 const [characters, setCharacters] = useState([]);
@@ -12,9 +13,8 @@ async function searchData(){
   const timestamp = new Date().getTime();
   const apiPrivateKey = 'deaf92ef7a6f4a0b8a552ba1221aaff469616519';
   const apiKeyPublic = '8c74855f312c29a14fda58143121f4d8';
-  const md5Response = await axios.get(`https://api.hashify.net/hash/md5/hex?value=${timestamp+apiPrivateKey+apiKeyPublic}`);
-  const md5 = md5Response.data.Digest;
-  const response = await axios.get(`https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${apiKeyPublic}&hash=${md5}`);
+  const md5Key = md5(timestamp+apiPrivateKey+apiKeyPublic);
+  const response = await axios.get(`https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&apikey=${apiKeyPublic}&hash=${md5Key}`);
   const list = response.data.data.results;
   setCharacters(list);
 }
