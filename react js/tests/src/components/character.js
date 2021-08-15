4a21cb2ecfce02e223b1dcd9791050c84b019ae5
jefflennon1 <jeffersonlennon1@outlook.com>
2021-08-15 11:55:55 -0300

    transição css que vai gostei

--- react js/tests/src/components/character.js
+++ react js/tests/src/components/character.js
@@ -43,12 +43,10 @@ export default function Character(props){
    }
 
    function handleMouseOverImage(idImage){
-     document.getElementById(idImage).style.transitionDuration = '6s'
-    /*  document.getElementById(idImage).style.width = '15em' */
+     document.getElementById(idImage).style.transitionDuration = '2s'
    }
 
    function handleMouseOutImage(idImage){
-    document.getElementById(idImage).style.transitionDuration = '6s'
-   /*  document.getElementById(idImage).style.width = '12.5em' */
-  }
+    document.getElementById(idImage).style.transitionDuration = '2s'
+    }
 }
